791dd5808752c4151a90d9220d458a81028f3075
Lukasz <lukasz.kaniak@gmail.com>
2020-06-14 23:00:27 +0200

    all platforms ok, firebase ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:ztiprojekt/abstracts/AppUser.dart';
 import 'package:ztiprojekt/mechanics/SignInEngine.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(MyApp());
@@ -47,42 +47,62 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: NetworkImage(_user.userPicUrl),
-              ),
-              Text(_user.userName),
-              Text(_user.userMail),
-              FlatButton(
-                onPressed: () {
-                  signInEngine.login();
-                },
-                color: Colors.green,
-                child: Text('Sign in with Google'),
-              ),
-              FlatButton(
-                onPressed: () {
-                  signInEngine.logout();
-                  setState(() {
-                    _user = AppUser.anonymous();
-                  });
-                },
-                color: Colors.red,
-                child: Text('Signout from Google'),
-              ),
-              FlatButton(
-                onPressed: () {
-                  setState(() {
-                    _user = signInEngine.GetUser();
-                  });
-                },
-                color: Colors.blue,
-                child: Text('GetData'),
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: StreamBuilder(
+                // <- To jest strumień z Firebase (Może być z czegokolwiek...)
+                stream: Firestore.instance.collection('tanks').snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) return CircularProgressIndicator();
+                  var snpErr = snapshot.hasError;
+                  var snpConn = snapshot.connectionState;
+                  var spsht = snapshot.data.documents;
+
+                  var tankName = spsht[0].data['name'];
+
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(_user.userPicUrl),
+                        ),
+                        Text(_user.userName),
+                        Text(_user.userMail),
+                        Text('$snpConn'),
+                        Text('$snpErr'),
+                        Text(tankName.toString()),
+                        FlatButton(
+                          onPressed: () {
+                            signInEngine.login();
+                          },
+                          color: Colors.green,
+                          child: Text('Sign in with Google'),
+                        ),
+                        FlatButton(
+                          onPressed: () {
+                            signInEngine.logout();
+                            setState(() {
+                              _user = AppUser.anonymous();
+                            });
+                          },
+                          color: Colors.red,
+                          child: Text('Signout from Google'),
+                        ),
+                        FlatButton(
+                          onPressed: () {
+                            setState(() {
+                              _user = signInEngine.GetUser();
+                            });
+                          },
+                          color: Colors.blue,
+                          child: Text('GetData'),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
           ),
         ),
       ),
